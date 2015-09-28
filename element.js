aba4fe6c3b131505d615265bde292f15b889cff9
Vashamillette Nick <nickv@live.be>
2015-09-28 12:01:31 +0200

    Commit test

--- element.js
+++ element.js
@@ -1,22 +1,31 @@
-var worldMap=5;
+var worldMap;
+var myCanvas; 
+var myContext;
 
 window.onload = function()
 {
-
-worldmapGen();
-
-
-}
-
-function worldmapGen(){
-	console.log("mapGen call");
-	
-	
-	
+myCanvas = document.getElementById("mon_canvas");
+myContext = myCanvas.getContext("2d");
+var worldMap = new myWorld();
+drawMap();
 }
 
 function myWorld() {
-  console.log(getMap("level_1"));
+	console.log('My world');
+	var mapData = getMap("level_1");
+	var mapTile = new Image();
+	mapTile.src = "tileset/"+mapData.tileset
+	console.log(mapTile);
+}
+
+function drawMap() {
+	for (x=0;x<20;x++)
+	{
+		for (y=0;y<20;y++){
+			//myContext.drawImage(worldMap.mapData.terrain[x][y],10,10)
+		}
+	}
+	
 }
 
 function getMap(map){
