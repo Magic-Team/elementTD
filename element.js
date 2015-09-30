afebe8ed4f194aa7279294c0b971df9bc046dd31
Vashamillette Nick <nickv@live.be>
2015-09-30 11:43:09 +0200

    BOUGE TON CUL POULAIN

--- element.js
+++ element.js
@@ -1,43 +1,28 @@
-var worldMap;
-var myCanvas; 
-var myContext;
 
-window.onload = function()
-{
-myCanvas = document.getElementById("mon_canvas");
-myContext = myCanvas.getContext("2d");
-worldMap = new myWorld();
-drawMap();
+var myWorld = new world("level_1");
+
+
+
+function tile(ba,bb,tn){
+var buildable;
+var building;
+var tileNumber;
+return this;
 }
 
-function myWorld() {
-	var mapTile;
-	var mapData;
-	this.mapData = getMap("level_1");
-	this.mapTile = new Image();
-	this.mapTile.src = "tileset/"+this.mapData.tileset
+function world(level){
+var life;
+var tileWorld;
+return this;
 }
 
-function drawMap() {
-	console.log(worldMap);
-	worldMap.mapTile.onload = function(){
-		for (x=0;x<20;x++)
-		{
-			for (y=0;y<20;y++){
-				myContext.drawImage(worldMap.mapTile,32*worldMap.mapData.terrain[y][x],32*worldMap.mapData.terrain[y][x],32,32,32*x,32*y,32,32);// img,sx,sy,swidth,sheight,x,y,width,height
-			}
-		}	
-	}
+function creep(){
+var id;
+var type;
+var hp;
+return this;
 }
 
-function getMap(map){
-	var xhr = new XMLHttpRequest()
-		xhr.open("GET", './level/' + map + '.json', false);
-		xhr.overrideMimeType("application/json");
-		xhr.send(null);
-		if(xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)) // Code == 0 en local
-			throw new Error("Impossible de charger la carte nommée \"" + nom + "\" (code HTTP : " + xhr.status + ").");
-		var mapJsonData = xhr.responseText;
-		var mapData = JSON.parse(mapJsonData);
-		return mapData;
+function building(){
+
 }
